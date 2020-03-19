dfe1a2dba14f37800bdfcf96ce2fac1667415c45
hzlnt <raulrusu46@gmail.com>
2020-03-19 16:39:13 +0200

    Added Firebase Auth functionality + a bit of refactor + added the Select Photo button with the circle drawable in the registration view

--- app/src/main/java/com/example/messengerapp/LoginActivity.kt
+++ app/src/main/java/com/example/messengerapp/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.example.messengerapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,12 +14,40 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_button_login.setOnClickListener {
-            val email = email_edittext_login.text.toString()
-            val password = password_edittext_login.text.toString()
-
-            Log.d("Login", "Logged in with: $email, $password")
+            performLogin()
         }
 
+        back_to_register_textview_login.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        // Check if inputs are empty.
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter your email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("Login", "Logged in with: $email, $password")
+
+        // Firebase Auth.
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val user = auth.currentUser
+                Log.d("Login", "logInWithEmail: success with ${user!!.uid}")
+
+            } else {
+                Log.w("Login", "logInWithEmail: failed", it.exception)
+                Toast.makeText(baseContext, "LogIn Failed", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Log.d("Login", "Failed to login: ${it.message}")
+        }
     }
 
 
--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +15,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("Register", "This is the mail: $email")
-            Log.d("Register", "This is the pw: $password")
+            performRegister()
         }
 
         already_have_account_text_view.setOnClickListener {
-            Log.d("Register", "Try to show log in activity")
+            Log.d("RegisterToLogin", "Try to show log in activity")
 
             // Watch the login activity somehow
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill the email / password text field!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("Register", "This is the mail: $email")
+        Log.d("Register", "This is the pw: $password")
+
+        // Firebase auth
+        val auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("Auth", "createUserWithEmail: success")
+                val user = auth.currentUser
+
+            } else {
+                Log.w("Auth", "createUserWithEmail: failed", it.exception)
+                Toast.makeText(baseContext, "Auth failed", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Log.d("Auth", "Failed to create user: ${it.message}")
         }
     }
 }
